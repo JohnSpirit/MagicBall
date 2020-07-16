ab5aa11ad7d144117156cf57f9d04f8f0a0adb51
JohnSpirit <JohnSpirit2001@gmail.com>
2020-07-16 11:01:17 +0800

    清理无用代码

--- MagicBall1/main.cpp
+++ MagicBall1/main.cpp
@@ -5,32 +5,5 @@ int main(void)
 {
 	Controller c(9, 9, HARD);
 	c.Play();
-
-	BALLTYPE b[] =
-	{
-		4,	5,	5,	5,	4,	5,	6,	0,	0,
-		0,	4,	0,	6,	0,	0,	6,	0,	0,
-		2,	0,	4,	3,	6,	0,	9,	4,	2,
-		0,	2,	0,	4,	2,	6,	6,	0,	1,
-		0,	4,	5,	0,	4,	0,	6,	0,	2,
-		0,	4,	0,	0,	4,	4,	4,	6,	0,
-		0,	0,	6,	0,	4,	0,	4,	0,	6,
-		0,	0,	0,	0,	5,	0,	0,	4,	0,
-		0,	0,	0,	0,	0,	0,	0,	0,	1
-	};
-	//BALLTYPE b[] =
-	//{
-	//	0,0,1,0,0,
-	//	0,0,1,2,3,
-	//	0,0,0,0,0,
-	//	0,0,0,0,0,
-	//	0,0,0,0,0
-	//};
-	Matrix<BALLTYPE> m(9, 9);
-	m.SetValueByArray(b, ALL);
-	cout << m.TypeCast<int>();
-	Board board(m);
-	cout << board.Check();
-	system("pause");
 	return 0;
 }
